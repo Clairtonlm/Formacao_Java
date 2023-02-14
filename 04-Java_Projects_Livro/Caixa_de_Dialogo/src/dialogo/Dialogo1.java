28f834dabc07a15f7d448027605a48929a728e5d
Clairtonlm <clairtonlm@gmail.com>
2023-02-14 13:36:43 -0300

    Iniciando projeto e estudo pelo livro Java

---
+++ 04-Java_Projects_Livro/Caixa_de_Dialogo/src/dialogo/Dialogo1.java
@@ -0,0 +1,9 @@
+package dialogo;
+
+import javax.swing.JOptionPane;
+
+public class Dialogo1 {
+    public static void main(String[] args) {
+        JOptionPane.showInputDialog("Welcome to Java");
+    }
+}
