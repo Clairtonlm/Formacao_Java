6d3798cecf6ae42d42f466b27eab8fe1e4fec2e2
Clairtonlm <clairtonln@gmail.com>
2023-02-15 16:00:08 -0300

    Removendo Disciplinas

---
+++ 01-Myschool-JDEV/src/EscolaVelha/TesteRun.java
@@ -0,0 +1,10 @@
+package EscolaVelha;
+
+public class TesteRun {
+
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo");
+
+	}
+
+}
--- 01-Myschool-JDEV/src/principalnovo/Aula9_21.java
+++ 01-Myschool-JDEV/src/principalnovo/Aula9_21.java
@@ -7,39 +7,41 @@ import escolanova.Materias;
 public class Aula9_21 {
 
 	public static void main(String[] args) {
-		
-		//Para receber as entradas
+
+		// Para receber as entradas
 		String nome = JOptionPane.showInputDialog("Qual o nome do Aluno");
 		String idade = JOptionPane.showInputDialog("Qual a idade do Aluno(a)");
-		
-		//1-Instancia o objeto
+
+		// 1-Instancia o objeto
 		Estudante estudante1 = new Estudante();
-		//2-seta os atributos
+		// 2-seta os atributos
 		estudante1.setNome(nome);
 		estudante1.setIdade(Integer.valueOf(idade));
-		
-		
-		  //Recebendo materia e nota de forma dinanmica
-		 for(int posicao = 1; posicao <= 4; posicao++) {
-			 String nomeMateria = JOptionPane.showInputDialog(" Nome da Materia " +posicao+ "?");
-			 String notaMateria = JOptionPane.showInputDialog(" Nota da Materia " +posicao+ "?");
-			 
-			 Materias materia = new Materias();
-			 materia.setMateria(nomeMateria);
-			 materia.setNota1(Double.valueOf(notaMateria));
-			 
-			 //Para Adcionar a lista
-			 estudante1.getMaterias().add(materia);
-		 }
-		 
-		
+
+		// Recebendo materia e nota de forma dinanmica
+		for (int posicao = 1; posicao <= 4; posicao++) {
+			String nomeMateria = JOptionPane.showInputDialog(" Nome da Materia " + posicao + "?");
+			String notaMateria = JOptionPane.showInputDialog(" Nota da Materia " + posicao + "?");
+
+			Materias materia = new Materias();
+			materia.setMateria(nomeMateria);
+			materia.setNota1(Double.valueOf(notaMateria));
+
+			// Para Adcionar a lista
+			estudante1.getMaterias().add(materia);
+		}
+
+		// Remover disciplina da Grade
+		int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma Materia");
+		if (escolha == 0) {
+			String materiaRemover = JOptionPane.showInputDialog("Qual materia a ser removida 1, 2, 3, 4 ? ");
+			estudante1.getMaterias().remove(Integer.valueOf(materiaRemover).intValue()-1);
+		}
+
 		System.out.println(estudante1);
 		System.out.println("Media do aluno = " + estudante1.getMediaNota());
 		System.out.println("Resultado = " + estudante1.getAlunoAprovado());
-		
-		
-		
-		
+
 	}
-	
+
 }
