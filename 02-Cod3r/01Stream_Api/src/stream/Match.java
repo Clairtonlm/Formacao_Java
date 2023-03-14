966512e9013f714326b05bfcc2707ed68d2c3404
Clairtonlm <Clairtonlm@gmail.com>
2023-03-14 01:34:45 -0300

    Trabalhando com metodos Match(All-any-none)

--- 02-Cod3r/01Stream_Api/src/stream/Match.java
+++ 02-Cod3r/01Stream_Api/src/stream/Match.java
@@ -1,10 +1,26 @@
 package stream;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Match {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		AlunoFilter a1 = new AlunoFilter("Zuroc", 7.5);
+		AlunoFilter a2 = new AlunoFilter("Ryan", 8.9);
+		AlunoFilter a3 = new AlunoFilter("Notrialc", 9.1);
+		AlunoFilter a4 = new AlunoFilter("Maty", 7.4);
+		
+		List<AlunoFilter> alunos = Arrays.asList(a1, a2, a3, a4);
+		
+		
+		Predicate<AlunoFilter> aprovados = a -> a.nota >=7;
+		Predicate<AlunoFilter> reprovados = aprovados.negate();
+		
+		System.out.println(alunos.stream().allMatch(aprovados));//todos foram aprovados
+		System.out.println(alunos.stream().anyMatch(aprovados));//algum foi aprovado
+		System.out.println(alunos.stream().noneMatch(reprovados));//tem aluno reprovado?
 	}
 
 }
