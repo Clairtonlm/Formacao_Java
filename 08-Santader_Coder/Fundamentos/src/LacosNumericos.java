8d9ccf6d66e30bbf8d864c992a85373eaf3971bc
Clairtonlm <clairtonlm@gmail.com>
2023-05-22 23:21:25 -0300

    Laco For

---
+++ 08-Santader_Coder/Fundamentos/src/LacosNumericos.java
@@ -0,0 +1,20 @@
+
+
+public class LacosNumericos {
+    public static void main(String[] args) {
+       //1 2 3 4 5 6 7 8 9 10
+    //para uma variavel que inicia em 1 e vai ate 10 mudando 1 por 1, faça:
+
+        for(int i = 1; i <= 10 ; i++){
+            for(int j =1; j <= 10; j++){
+                System.out.println(j + " x " + i + " = " + j * i);
+            }
+
+            
+            
+
+        }
+        
+    }//main
+    
+}//fim da class
