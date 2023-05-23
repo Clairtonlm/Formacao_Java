a72a76cc3ec9f3baa4795bb79ac1c96059a2ca1b
Clairtonlm <clairtonlm@gmail.com>
2023-05-23 00:04:50 -0300

    metodos

---
+++ 08-Santader_Coder/Fundamentos/src/Funcoes.java
@@ -0,0 +1,17 @@
+
+
+public class Funcoes {
+    public static void main(String[] args) {
+        String nomeOriginal = "Dev";
+        saudacao(nomeOriginal);
+
+
+
+    }//main
+
+    public static void saudacao(String nomeParamentro){
+        System.out.println("Hello " + nomeParamentro);
+    }
+
+    
+}//fim class
---
+++ 08-Santader_Coder/Fundamentos/src/Soma.java
@@ -0,0 +1,17 @@
+
+
+public class Soma {
+    public static void main(String[] args) {
+        
+        int resultado = soma(1, 3);
+        System.out.println(resultado);
+
+
+    }
+
+    public static int soma(int x, int y){
+        return x + y;
+    }
+
+
+}
