d9fa50c6b7c4e3fe749986258f95d62b5325f9d1
Clairtonlm <clairtonlm@gmail.com>
2023-05-23 23:24:53 -0300

    Metodos

--- 08-Santader_Coder/CursoPoo/src/animais/Cachorro.java
+++ 08-Santader_Coder/CursoPoo/src/animais/Cachorro.java
@@ -1,12 +1,37 @@
 package animais;
 
 public class Cachorro {
+    //atributos
     public String      nome;
     public String      cor;
     public int         altura;
     public double      peso;
+    public String      estadoDeEspirito;
 
+// metodos semretorno
+public void comer() {
+    System.out.println("Comeu ração");
+}
+public void latir() {
+    System.out.println("AU AU");
+}
 
-
+//metodo com retorno. e o retorno tem que ser do mesmo tipo
+public String pegar(){
+    return "Bolinha";
+}
+public String interagir(String acao) {
+    if(acao.equalsIgnoreCase("carinho")){
+        this.estadoDeEspirito = "feliz";
+    }else if(acao.equalsIgnoreCase("vai dormir")){
+        this.estadoDeEspirito = " Bravo";
+    } else{
+        this.estadoDeEspirito = " neutro";
+    }
+        return estadoDeEspirito; 
 
 }
+
+
+
+}//fim da class cacchorro
--- 08-Santader_Coder/CursoPoo/src/animais/Main.java
+++ 08-Santader_Coder/CursoPoo/src/animais/Main.java
@@ -1,7 +1,9 @@
 package animais;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Cachorro cachorro1 = new Cachorro();
         cachorro1.nome= "Banby";
         cachorro1.cor= "Preto";
@@ -9,8 +11,14 @@ public class Main {
         cachorro1.altura= 5;
         System.out.println(cachorro1);
 
+        cachorro1.latir();
+        System.out.println("cachorro pegou uma " + cachorro1.pegar());
+        System.out.println("O cachorro está " + cachorro1.interagir("Carinho"));
+        System.out.println("O cachorro está " + cachorro1.interagir("vai dormir"));
+
 
 
 
     }//main
+
 }//fim main
