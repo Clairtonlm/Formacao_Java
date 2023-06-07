182e262c3b96abf0bb8988a766f0dcbe9effb653
Clairtonlm <clairtonlm@gmail.com>
2023-06-06 23:17:47 -0300

    Fundamentos

---
+++ 09-JavaOne/00-Fundamentos/src/fundamentos/TestaVariaveis.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		
+		
+		int idade = 37;
+		System.out.println( "A idade digitada foi --> " + idade);
+		
+	}//fim do Main
+
+}//fim da Classe
