e43135eea58acf78ef49b6dfc33ef61248fc8ce0
Clairtonlm <clairtonlm@gmail.com>
2023-06-07 15:00:00 -0300

    Cast em java

---
+++ 09-JavaOne/00-Fundamentos/src/fundamentos/Conversao.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class Conversao {
+
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		int valor = (int)salario;
+		System.out.println(valor);
+	}
+
+}
---
+++ 09-JavaOne/00-Fundamentos/src/fundamentos/TestaPontoFlutuante.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		double salario = 1234.5;
+		System.out.println("Meu salário é " + salario);
+	}
+
+}
