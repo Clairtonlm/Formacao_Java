36365065e22d85774fb7c34500c52391a64355c5
Clairtonlm <clairtonlm@gmail.com>
2023-06-07 21:23:20 -0300

    Crinado Banco Digital

---
+++ 09-JavaOne/00-Fundamentos/src/fundamentos/Caracteres.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class Caracteres {
+
+	public static void main(String[] args) {
+		char letra	 = 'C';
+		System.out.println(letra);
+	}
+
+}
--- 09-JavaOne/00-Fundamentos/src/fundamentos/Conversao.java
+++ 09-JavaOne/00-Fundamentos/src/fundamentos/Conversao.java
@@ -5,6 +5,9 @@ public class Conversao {
 	public static void main(String[] args) {
 		double salario = 1270.50;
 		int valor = (int)salario;
+		long numeroGrande = 32232323232323232l;
+		short valorPequeno = 32000;
+		byte b = 127;
 		System.out.println(valor);
 	}
 
