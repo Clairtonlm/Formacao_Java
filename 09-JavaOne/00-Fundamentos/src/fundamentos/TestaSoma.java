d24ec2ca21d731c370c2dfda63ed355ebf4e114d
Clairtonlm <clairtonlm@gmail.com>
2023-06-08 22:43:47 -0300

    For em java

---
+++ 09-JavaOne/00-Fundamentos/src/fundamentos/TestaSoma.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class TestaSoma {
+
+	public static void main(String[] args) {
+		int contador = 0;
+		int total = 0;
+		while(contador <=10) {
+			total += contador;
+			
+			contador++;
+		}
+		System.out.println(total);
+	}
+
+}
