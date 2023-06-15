b37365563be708215e93c5d392c94b0f783051d2
Clairtonlm <clairtonlm@gmail.com>
2023-06-14 22:43:01 -0300

    Definindo metodos

--- 09-JavaOne/02-OrientacaoObjetos/src/bytebanck1/Conta.java
+++ 09-JavaOne/02-OrientacaoObjetos/src/bytebanck1/Conta.java
@@ -9,6 +9,25 @@ public class Conta {
 	
 	//Metodos
 	 public void deposita( double valor){
-		 saldo = saldo + valor;
+		 this.saldo += valor;
+	}
+	 
+	public boolean saca(double valor){
+		 if(this.saldo >= valor) {
+			 this.saldo -= valor;
+			 return true;
+		 }else {
+			 return false;
+		 }
+	 }
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			//destisno.saldo += valor;(ou pode ser usando o metodo)
+			destino.deposita(valor);
+			return true;
+		}else {
+			return false;
+		}
 	}
 }
--- 09-JavaOne/02-OrientacaoObjetos/src/bytebanck1/TestaMetodo.java
+++ 09-JavaOne/02-OrientacaoObjetos/src/bytebanck1/TestaMetodo.java
@@ -4,9 +4,30 @@ public class TestaMetodo {
 
 	public static void main(String[] args) {
 		Conta conta = new Conta();
-		conta.saldo = 200;
-		conta.deposita(100);
+		System.out.println("---Conta cliente 1---");
+		conta.saldo = 920;//saldo inicial
+		conta.deposita(100);//depositei
+		System.out.println(conta.saldo);//920 + 100 = 1020
+		boolean fezSaque = conta.saca(2000);// saque 
+		System.out.println(conta.saldo);//1020-20.80 = 999.2
+		System.out.println(fezSaque);
+		
+		//Nova conta
+		System.out.println("---Conta cliente 2---");
+		Conta conta2 = new Conta();
+		conta2.saldo = 1000;
+		System.out.println(conta2.saldo);
+		
+		//invocando o medoto transfere
+		if(conta2.transfere(1001, conta)) {
+			System.out.println("Transferência feita com sucesso!");
+		}else {
+			System.out.println("Saldo insuficiente, transfência nao relaizada");
+		}
+		
+		System.out.println(conta2.saldo);
 		System.out.println(conta.saldo);
+		
 	}
 
 }
