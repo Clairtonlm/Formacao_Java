6bcea3ac0f47e5ce72b5c8b3ed7112121d336b72
Clairtonlm <clairtonlm@gmail.com>
2023-06-18 15:26:42 -0300

    Iniciando Polimorfismo

--- 09-JavaOne/04-BayteBank-Herdado/src/Funcionario.java
+++ 09-JavaOne/04-BayteBank-Herdado/src/Funcionario.java
@@ -2,7 +2,7 @@
 public class Funcionario {
 	private String nome;
 	private String cpf;
-	protected double salario;
+	private double salario;
 	
 	public double getBonificacao() {
 		return this.salario * 0.5;
---
+++ 09-JavaOne/04-BayteBank-Herdado/src/TesteReferencia.java
@@ -0,0 +1,12 @@
+
+public class TesteReferencia {
+
+	public static void main(String[] args) {
+		Funcionario g1 = new Gerente();
+		g1.setNome("Noah Lima");
+		
+		g1.imprimir();
+
+	}
+
+}
