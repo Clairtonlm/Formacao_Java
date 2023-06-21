93a5e1d654f0560dcc314dbdb1cc565bbbbaaa45
Clairtonlm <clairtonlm@gmail.com>
2023-06-20 23:27:54 -0300

    Praticando Interfaces

---
+++ 09-JavaOne/05-batybank-herdado-conta/src/TesteTributavel.java
@@ -0,0 +1,18 @@
+
+public class TesteTributavel {
+
+	public static void main(String[] args) {
+		ContaCorrente cc = new ContaCorrente(222, 1070);
+		cc.deposita(100);
+		
+		SeguroDeVida seguro = new SeguroDeVida();
+		
+		CalcularImposto calc = new CalcularImposto();
+		calc.registra(cc);
+		calc.registra(seguro);
+		
+		System.out.println(calc.getTotalImposto());
+		
+	}
+
+}
