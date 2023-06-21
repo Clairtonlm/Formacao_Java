64fdcf637f7df08aa56ba6f504ef4074b02afcd3
Clairtonlm <clairtonlm@gmail.com>
2023-06-20 23:02:04 -0300

    Composição

--- 09-JavaOne/04-BayteBank-Herdado/src/Admintrador.java
+++ 09-JavaOne/04-BayteBank-Herdado/src/Admintrador.java
@@ -1,20 +1,21 @@
 
 public class Admintrador extends Funcionario implements Autenticavel {
 
-	private int senha = 3333;
+	private AutenticacaoUtil autenticador;
+
+
+	public Admintrador() {
+		this.autenticador = new AutenticacaoUtil();
+	}
 	
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
+		this.autenticador.setSenha(senha);
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		}else {
-			return false;
-		}
+		return this.autenticador.autentica(senha);
 		
 	}
 	
