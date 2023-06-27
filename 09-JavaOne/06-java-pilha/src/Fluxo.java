b60347e14c797dd9ce67f220e150c2a1dfabebf6
Clairtonlm <clairtonlm@gmail.com>
2023-06-26 23:41:23 -0300

    Lancanco excecao Try/ Catch

--- 09-JavaOne/06-java-pilha/src/Fluxo.java
+++ 09-JavaOne/06-java-pilha/src/Fluxo.java
@@ -8,13 +8,20 @@ public class Fluxo {
 	}
 	private static void metodo1() {
 		System.out.println("Ini do metodo1");
-		metodo2();
+		try {
+			metodo2();
+		}catch(ArithmeticException ex) {
+			System.out.println("ArithmeticException");
+		}
+		
 		System.out.println("Fim do medoto1");
 	}
 	private static void metodo2() {
 		System.out.println("Ini do metodo2");
 		for(int i = 1; i <= 5; i++) {
 			System.out.println(i);
+			int a = i / 0;
+			
 		}
 		System.out.println("Fim do medoto2");
 	}
