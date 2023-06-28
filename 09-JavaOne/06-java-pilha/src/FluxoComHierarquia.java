81018c16131d090de39f3df9373112acb42c457f
Clairtonlm <clairtonlm@gmail.com>
2023-06-27 22:54:06 -0300

    Criando exceções

--- 09-JavaOne/06-java-pilha/src/FluxoComHierarquia.java
+++ 09-JavaOne/06-java-pilha/src/FluxoComHierarquia.java
@@ -1,11 +1,11 @@
 
-public class FluxoComTratamento {
+public class FluxoComHierarquia {
 
 	public static void main(String[] args) {
 		System.out.println("Inicio do main");
 		try {
 			metodo1();
-		}catch(ArithmeticException | NullPointerException |MinhaExcecao ex) {
+		}catch(ArithmeticException | NullPointerException |MinhaExcecao2 ex) {
 			String msg = ex.getMessage();
 			System.out.println("Exception " + msg);
 			ex.printStackTrace();
@@ -13,16 +13,16 @@ public class FluxoComTratamento {
 		
 		System.out.println("Fim do main");
 	}
-	private static void metodo1() {
+	private static void metodo1() throws MinhaExcecao2 {
 		System.out.println("Ini do metodo1");
 			metodo2();
 		System.out.println("Fim do medoto1");
 	}
-	private static void metodo2() {
+	private static void metodo2() throws MinhaExcecao2 {
 		System.out.println("Ini do metodo2");
 		
 		//criando objeto 
-		throw new MinhaExcecao("Deu errado");
+		throw new MinhaExcecao2("Deu errado");
 		
 		
 		//System.out.println("Fim do medoto2");
---
+++ 09-JavaOne/06-java-pilha/src/MinhaExcecao2.java
@@ -0,0 +1,7 @@
+
+public class MinhaExcecao2 extends RuntimeException {
+	
+	public MinhaExcecao2(String msg) {
+		super(msg);
+	}
+}
