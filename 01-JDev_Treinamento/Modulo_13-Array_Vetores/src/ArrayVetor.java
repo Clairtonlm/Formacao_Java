c0c0299eeba7696443120d9b0bf26bade19efcb2
Clairtonlm <clairtonlm@gmail.com>
2023-07-05 22:52:12 -0300

    Como criar um array

--- 01-JDev_Treinamento/Modulo_13-Array_Vetores/src/ArrayVetor.java
+++ 01-JDev_Treinamento/Modulo_13-Array_Vetores/src/ArrayVetor.java
@@ -1,4 +1,17 @@
 
 public class ArrayVetor {
-
+	 
+	public static void main(String[] args) {
+		
+		/*Array pode ser de todos os tipos de dados e objetos */
+		/* Array deve ter a quantidade de posições definidas*/
+		double[] notas = new double[5];
+		
+		notas[0]=9.8;
+		notas[1]=7.6;
+		notas[2]=8.7;
+		notas[3]=9.9;
+		
+		System.out.println(notas[0]);
+	}
 }
