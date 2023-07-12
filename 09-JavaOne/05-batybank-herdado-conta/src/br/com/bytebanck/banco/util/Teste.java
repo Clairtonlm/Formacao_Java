ce95745a7b0ab7312def65099c649f8977fdb88e
Clairtonlm <clairtonlm@gmail.com>
2023-07-11 23:39:07 -0300

    Comparando

--- 09-JavaOne/05-batybank-herdado-conta/src/br/com/bytebanck/banco/util/Teste.java
+++ 09-JavaOne/05-batybank-herdado-conta/src/br/com/bytebanck/banco/util/Teste.java
@@ -1,6 +1,7 @@
 package br.com.bytebanck.banco.util;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import br.com.bytebanck.banco.modelo.Conta;
@@ -26,7 +27,42 @@ public class Teste {
 		lista.add(cc2);
 		lista.add(cc3);
 		lista.add(cc4);
+		
+		//5- fazer um laço antes da comparação
+		for (Conta conta : lista) {
+			System.out.println(conta);
+		}
+		
+		
+		//3- instancia a classe
+		NumeroDaContaComparator comparador = new NumeroDaContaComparator();
+		lista.sort(comparador);
+		System.out.println("-----------------------------");
+		
+		//6- fazer um laço depois da comparação
+				for (Conta conta : lista) {
+					System.out.println(conta);
+				}
+		
+		//4-passa para o metodo de ordenação da lista
+		
 
 	}
 
+}
+//1-cria a classe com implementes
+class NumeroDaContaComparator implements Comparator<Conta>{
+
+	//2-faz o criterio do metodo
+	@Override
+	public int compare(Conta c1, Conta c2) {
+		if(c1.getNumero() < c2.getNumero()) {
+			return -1;//qual coisa aqui
+		}
+		if(c1.getNumero() > c2.getNumero()) {
+			return 1;//qualquer coisa
+		}
+		return 0;
+	}
+	
 }
