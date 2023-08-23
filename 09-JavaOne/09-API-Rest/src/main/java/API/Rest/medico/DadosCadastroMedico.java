d0f349d7a1a31dd3e8a9a79decd47288863e6395
Clairtonlm <clairtonlm@gmail.com>
2023-08-22 22:17:51 -0300

    Conectando ao Mysql

--- 09-JavaOne/09-API-Rest/src/main/java/API/Rest/medico/DadosCadastroMedico.java
+++ 09-JavaOne/09-API-Rest/src/main/java/API/Rest/medico/DadosCadastroMedico.java
@@ -2,5 +2,7 @@ package API.Rest.medico;
 
 import API.Rest.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+//record classe mutavel
+public record DadosCadastroMedico
+        (String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
 }
