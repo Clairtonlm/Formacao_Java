242d09f72cda56564d379ad005ff81fcf3846e99
Clairtonlm <clairtonlm@gmail.com>
2023-09-28 22:40:48 -0300

    Iniciando DevDojojava

---
+++ 06-Pratica_Java/LogicaProgramacaoBasica/src/logica/Logica.java
@@ -0,0 +1,13 @@
+package logica;
+
+public class Logica {
+    public static void main(String[] args) {
+        int idade = 16;
+        if (idade >= 18){
+            System.out.println("pode dirigir");
+        }else{
+            System.out.println("Nao pode dirigir é menor");
+        }
+    }
+
+}
---
+++ 14-Java_Dojo/Aula17-OperadoresLogicos/src/OperadoresLogicos.java
@@ -0,0 +1,11 @@
+public class OperadoresLogicos {
+    //  && (and) || (or) !
+    public static void main(String[] args) {
+        int idade = 35;
+        float salario = 3500F;
+        boolean isDentroDalei = idade > 30 && salario > 4612;
+        System.out.println(isDentroDalei);
+    }
+
+
+}
