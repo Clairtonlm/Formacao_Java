0a1b362db4a646d4f86c8e8dc5d619c5ccafadd0
Clairtonlm <clairtonlm@gmail.com>
2023-10-01 22:36:17 -0300

    Teste de Mesa

---
+++ 03-CursoJavaNA/01-Basico/src/modulo/estruturarepetitivas/Testemesa.java
@@ -0,0 +1,16 @@
+package modulo.estruturarepetitivas;
+
+public class Testemesa {
+
+	public static void main(String[] args) {
+		int x = 0;
+		int y = 4;
+		
+		while(x < 3) {
+			y = y + 2;
+			x = x + 1;
+			System.out.println(x + "-" + y);
+		}
+	}
+
+}
