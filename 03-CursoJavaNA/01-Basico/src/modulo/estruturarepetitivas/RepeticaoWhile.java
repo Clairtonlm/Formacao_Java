6f3e7d9bbee964e1d567c24bb4b00eaa0fa1dddc
Clairtonlm <clairtonlm@gmail.com>
2023-10-01 22:26:08 -0300

    Repeticao While em java

---
+++ 03-CursoJavaNA/01-Basico/src/modulo/estruturarepetitivas/RepeticaoWhile.java
@@ -0,0 +1,27 @@
+package modulo.estruturarepetitivas;
+
+import java.util.Scanner;
+
+public class RepeticaoWhile {
+/*
+ * fazer um programa que le numeros inteiros ate que um zero seja lido. Ao final mostrar a soma dos numeros lidos
+ * */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o número");
+		int x = sc.nextInt();
+		int soma = 0;
+		
+		while(x != 0) {
+			soma = soma + x;
+			System.out.println("Digite o número");
+			x = sc.nextInt();
+		}
+		System.out.println(soma);
+		
+		sc.close();
+	}
+
+}
+
+
