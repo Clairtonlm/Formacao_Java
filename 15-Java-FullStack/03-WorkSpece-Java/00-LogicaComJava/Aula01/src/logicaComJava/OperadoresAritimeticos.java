f1a1eca17f7704c67f9d39edd9953f8830912fe2
Clairtonlm <clairtonlm@gmail.com>
2024-01-06 20:44:00 -0300

    Operadores relacionais

--- 15-Java-FullStack/03-WorkSpece-Java/00-LogicaComJava/Aula01/src/logicaComJava/OperadoresAritimeticos.java
+++ 15-Java-FullStack/03-WorkSpece-Java/00-LogicaComJava/Aula01/src/logicaComJava/OperadoresAritimeticos.java
@@ -6,6 +6,7 @@ public class OperadoresAritimeticos {
 		
 		double num1 = 80;
 		double num2 = 50;
+		
 		System.out.println("Adição:" + (num1 + num2));
 		System.out.println("Subtração:" + (num1 - num2));
 		System.out.println("Multiplicação:" + (num1 * num2));
---
+++ 15-Java-FullStack/03-WorkSpece-Java/00-LogicaComJava/Aula01/src/logicaComJava/OperadoresRelacionais.java
@@ -0,0 +1,21 @@
+package logicaComJava;
+
+public class OperadoresRelacionais {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = 10;
+		
+		if(num1 == num2) {
+			System.out.println(" Números são iguais");
+		}else if(num1 != num2){
+			System.out.println("Ganhou mais ");
+		
+		}else {
+			System.out.println("Nada");
+		}
+		
+	}
+
+}
