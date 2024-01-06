e7578896be6fe374b324238374e8d02cfc279c05
Clairtonlm <clairtonlm@gmail.com>
2024-01-06 20:26:40 -0300

    Operadores Aritimeticos

---
+++ 15-Java-FullStack/03-WorkSpece-Java/00-LogicaComJava/Aula01/src/logicaComJava/OperadoresAritimeticos.java
@@ -0,0 +1,16 @@
+package logicaComJava;
+
+public class OperadoresAritimeticos {
+
+	public static void main(String[] args) {
+		
+		double num1 = 80;
+		double num2 = 50;
+		System.out.println("Adição:" + (num1 + num2));
+		System.out.println("Subtração:" + (num1 - num2));
+		System.out.println("Multiplicação:" + (num1 * num2));
+		System.out.println("Divisão:" + (num1 / num2));
+		System.out.println("modulo:" + (num1 % num2));
+	}
+
+}
