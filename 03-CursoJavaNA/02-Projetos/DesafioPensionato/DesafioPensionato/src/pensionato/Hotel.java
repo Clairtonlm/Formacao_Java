0569edf25bc94a15aae7c05e1cc22cde2d29d1f0
Clairtonlm <clairtonlm@gmail.com>
2024-01-16 21:40:13 -0300

    Finalizando Alguel de quartos no hotel

--- 03-CursoJavaNA/02-Projetos/DesafioPensionato/DesafioPensionato/src/pensionato/Hotel.java
+++ 03-CursoJavaNA/02-Projetos/DesafioPensionato/DesafioPensionato/src/pensionato/Hotel.java
@@ -8,18 +8,32 @@ public class Hotel {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Rent r = new Rent("maria", "maria@gmail.com");
-        System.out.println(r);
+        Rent[] vect = new Rent[10];
 
+        System.out.println(" How many rooms will be rented? ");
+        int n = sc.nextInt();
 
+        for(int i = 1; i <= n; i++){
+            System.out.println();
+            System.out.println("Rent #" + i + ":");
+            System.out.print("Digite seu Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Qual seu Email: ");
+            String email = sc.next();
+            System.out.print("Room: Temos quartos de 0 a 9. Qual vc quer? ");
+            int roomNumber = sc.nextInt();
 
-
-
-
-
-
-
-
+            Rent rent = new Rent(name, email);
+            vect[roomNumber] = rent;
+        }
+        System.out.println();
+        System.out.println("Busy rooms");
+        for(int i = 0; i< 10; i++){
+            if(vect[i] != null){
+                System.out.println(i + ": " + vect[i]);
+            }
+        }
 
 
         sc.close();
