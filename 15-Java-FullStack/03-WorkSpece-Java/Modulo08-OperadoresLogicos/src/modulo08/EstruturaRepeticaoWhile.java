1f2a62bf0077a3546c1adc26ef2e0e9aa5c128d4
Clairtonlm <clairtonlm@gmail.com>
2024-01-17 23:13:58 -0300

    Estrutura de repetição Whule e Do While

---
+++ 15-Java-FullStack/03-WorkSpece-Java/Modulo08-OperadoresLogicos/src/modulo08/EstruturaRepeticaoWhile.java
@@ -0,0 +1,11 @@
+package modulo08;
+//Estrutura de repetição While- Repetir enquanto uma condição for verdadeira
+public class EstruturaRepeticaoWhile {
+    public static void main(String[] args) {
+        int num = 0;
+        while (num <=10){
+            System.out.println("O número atual é " + num);
+            num++;
+        }
+    }
+}
