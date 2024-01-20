071f59d0e31a76679d056499588783b699927566
Clairtonlm <clairtonlm@gmail.com>
2024-01-19 23:46:48 -0300

    Media Alunos

--- 15-Java-FullStack/03-WorkSpece-Java/Modulo08-OperadoresLogicos/src/modulo08/EntradaDados.java
+++ 15-Java-FullStack/03-WorkSpece-Java/Modulo08-OperadoresLogicos/src/modulo08/EntradaDados.java
@@ -6,10 +6,24 @@ public class EntradaDados {
     public static void main(String[] args) {
         String nome = JOptionPane.showInputDialog("Informe seu nome");
         System.out.println(nome);
-        String idade = JOptionPane.showInputDialog("Qual a sua idade");
-        System.out.println(idade);
+        String carro = JOptionPane.showInputDialog("quantos carros");
+        System.out.println(carro);
+        String pessoa = JOptionPane.showInputDialog("quantas pessoas");
+        System.out.println(pessoa);
 
-        //converter esses dados
-        double idadeNumero = Double.parseDouble(idade);
+        //converter esses dados string em double
+        double carroNumero = Double.parseDouble(carro);
+        double pessoaNumero = Double.parseDouble(pessoa);
+
+        // converto double em inteiro na divisão
+        int divisao = (int)(carroNumero / pessoaNumero);
+        System.out.println(divisao);
+        double resto = carroNumero % pessoaNumero;
+
+       int res = JOptionPane.showConfirmDialog(null, "Quer o resultado da divsão");
+       if(res == 0){
+          JOptionPane.showMessageDialog(null,"A divisão foi de " + divisao + " E resto de " + resto);
+       }
+        System.out.println(resto);
     }
 }
