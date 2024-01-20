d6c625688cd8472d28bbc493eb49a0e0abbfafbe
Clairtonlm <clairtonlm@gmail.com>
2024-01-19 23:03:21 -0300

    Entrada de dados

---
+++ 15-Java-FullStack/03-WorkSpece-Java/Modulo08-OperadoresLogicos/src/modulo08/RestoDivisao.java
@@ -0,0 +1,12 @@
+package modulo08;
+
+public class RestoDivisao {
+    public static void main(String[] args) {
+        double num = 9;
+        if(num % 2 == 0){
+            System.out.println("N é par");
+        }else {
+            System.out.println("Não é par");
+        }
+    }
+}
