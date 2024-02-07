f1748f267d31cf95e0c4d738f9b5990b733f1acc
Clairtonlm <clairtonlm@gmail.com>
2024-02-06 22:31:22 -0300

    Usando metodos

--- 15-Java-FullStack/03-WorkSpece-Java/Modulo09/Modulo09/src/Aluno95.java
+++ 15-Java-FullStack/03-WorkSpece-Java/Modulo09/Modulo09/src/Aluno95.java
@@ -2,9 +2,15 @@ package Modulo09.Modulo09.src;
 
 public class Aluno95 {
     private String nome;
-    private int idade;
+    private int    idade;
     private String dataNascimento;
     private String cpf;
+
+    private double nota1;
+    private double nota2;
+    private double nota3;
+    private double nota4;
+
 
     //Construtores em Java
     public Aluno95(){//cria os dados na memoria - sendo padrão no java
@@ -49,5 +55,42 @@ public class Aluno95 {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+
+    public double getNota4() {
+        return nota4;
+    }
+
+    public void setNota4(double nota4) {
+        this.nota4 = nota4;
+    }
+    //Métodos
+    /* Metodo que calcula nota do aluno*/
+    public double mediaAluno(){
+        return (nota1 + nota2 + nota3 + nota4)/4;
     }
 }//classe
--- 15-Java-FullStack/03-WorkSpece-Java/Modulo09/Modulo09/src/Main.java
+++ 15-Java-FullStack/03-WorkSpece-Java/Modulo09/Modulo09/src/Main.java
@@ -8,6 +8,11 @@ public class Main {
         aluno1.setIdade(52);
         aluno1.setDataNascimento("10/02/2025");
         aluno1.setCpf("002.005.003-04");
+        aluno1.setNota1(9.2);
+        aluno1.setNota2(7.8);
+        aluno1.setNota3(8.9);
+        aluno1.setNota4(9.5);
+
 
         System.out.println("Aluno 02");
         Aluno95 aluno2 = new Aluno95();// Aqui é o pedro
@@ -24,6 +29,7 @@ public class Main {
         Aluno95 aluno5 = new Aluno95("Maria", 23);
 
         System.out.println("O nome do Aluno 01 é " + aluno1.getNome());
+        System.out.println("A média do da nota é = " + aluno1.mediaAluno());
     }
 
 
