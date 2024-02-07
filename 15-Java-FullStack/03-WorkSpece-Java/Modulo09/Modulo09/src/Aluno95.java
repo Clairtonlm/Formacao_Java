ff7656bb5995bf7c041c1839d1365cbb75521cd8
Clairtonlm <clairtonlm@gmail.com>
2024-02-06 21:52:36 -0300

    Trabalhando com Getters - Recupera os dados e Setters- Adiciona os dados para atrinutos

--- 15-Java-FullStack/03-WorkSpece-Java/Modulo09/Modulo09/src/Aluno95.java
+++ 15-Java-FullStack/03-WorkSpece-Java/Modulo09/Modulo09/src/Aluno95.java
@@ -1,10 +1,10 @@
 package Modulo09.Modulo09.src;
 
 public class Aluno95 {
-     String nome;
-    int idade;
-    String dataNascimento;
-    String cpf;
+    private String nome;
+    private int idade;
+    private String dataNascimento;
+    private String cpf;
 
     //Construtores em Java
     public Aluno95(){//cria os dados na memoria - sendo padrão no java
@@ -17,4 +17,37 @@ public class Aluno95 {
         this.nome = nomePadrao;
         this.idade = idadePadrao;
     }
-}
+
+    //Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}//classe
--- 15-Java-FullStack/03-WorkSpece-Java/Modulo09/Modulo09/src/Main.java
+++ 15-Java-FullStack/03-WorkSpece-Java/Modulo09/Modulo09/src/Main.java
@@ -4,10 +4,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Aluno 01");
         Aluno95 aluno1 = new Aluno95();// Aqui é joão
-        aluno1.nome = "João";
+        aluno1.setNome("João");
+        aluno1.setIdade(52);
+        aluno1.setDataNascimento("10/02/2025");
+        aluno1.setCpf("002.005.003-04");
 
         System.out.println("Aluno 02");
         Aluno95 aluno2 = new Aluno95();// Aqui é o pedro
+
+
 
         System.out.println("Aluno 03");
         Aluno95 aluno3 = new Aluno95();// Aqui é Zayron
@@ -18,7 +23,7 @@ public class Main {
         System.out.println("Aluno 05");
         Aluno95 aluno5 = new Aluno95("Maria", 23);
 
-        System.out.println(aluno1.nome);
+        System.out.println("O nome do Aluno 01 é " + aluno1.getNome());
     }
 
 
