5391d26fea803a51a898bf5c60d2a619ef23611b
Clairtonlm <clairtonlm@gmail.com>
2024-02-09 14:37:41 -0300

    Arrumando

--- 15-Java-FullStack/03-WorkSpece-Java/Modulo09/Modulo09/src/Main.java
+++ 15-Java-FullStack/03-WorkSpece-Java/Modulo09/Modulo09/src/Main.java
@@ -9,21 +9,23 @@ public class Main {
         Aluno95 aluno1 = new Aluno95();// Aqui é joão
 
         String nome = JOptionPane.showInputDialog("Digite o nome do Aluno: ");
-        aluno1.setNome(nome);
         String idade = JOptionPane.showInputDialog("Qual a idade do aluno:");
-        aluno1.setIdade(Integer.valueOf(idade));
         String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento do aluno: ");
-        aluno1.setDataNascimento(dataNascimento);
         String cpf = JOptionPane.showInputDialog("Qual a CPF do aluno: ");
+
+        aluno1.setNome(nome);
+        aluno1.setIdade(Integer.valueOf(idade));
+        aluno1.setDataNascimento(dataNascimento);
         aluno1.setCpf(cpf);
 
         String nota1 = JOptionPane.showInputDialog("Digite a nota 1 do Aluno: ");
-        aluno1.setNota1(Double.parseDouble(nota1));
         String nota2 = JOptionPane.showInputDialog("Digite a nota 2 do Aluno: ");
-        aluno1.setNota2(Double.parseDouble(nota2));
         String nota3 = JOptionPane.showInputDialog("Digite a nota 3 do Aluno: ");
-        aluno1.setNota3(Double.parseDouble(nota3));
         String nota4 = JOptionPane.showInputDialog("Digite a nota 4 do Aluno: ");
+
+        aluno1.setNota1(Double.parseDouble(nota1));
+        aluno1.setNota2(Double.parseDouble(nota2));
+        aluno1.setNota3(Double.parseDouble(nota3));
         aluno1.setNota4(Double.parseDouble(nota4));
 
         System.out.println(aluno1.toString());
@@ -35,7 +37,6 @@ public class Main {
         System.out.println("A data de nascimento do Aluno 01 é: " + aluno1.getDataNascimento());
         System.out.println("O CPF do Aluno 01 é: " + aluno1.getCpf());
         System.out.println("A média do Aluno é = " + aluno1.mediaAluno());*/
-
         //System.out.println("Resultado do aluno = " + (aluno1.verificaAprovacao() ? "APROVADO" : "REPROVADO"));
         //System.out.println("O aluno está " + aluno1.verificaAprovacao2());
 
