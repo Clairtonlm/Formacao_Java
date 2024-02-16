8122c7d6e76f7fb9aa353fba8f78f2b54add1e4e
Clairtonlm <clairtonlm@gmail.com>
2024-02-16 09:53:44 -0300

    Criando estrutura do projeto

---
+++ 03-CursoJavaNA/08-ComposicaoObjetos/src/enums/workerLevel.java
@@ -0,0 +1,9 @@
+package src.enums;
+
+public enum workerLevel {
+    JUNIOR,
+    MID_LEVEL,
+    PLENO,
+    SENIOR
+
+}
---
+++ 03-CursoJavaNA/08-ComposicaoObjetos/src/program/Main.java
@@ -0,0 +1,7 @@
+package src.program;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
