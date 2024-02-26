fc9e8c110ebb275fdec88295ef1c8e9cbc1b7694
Clairtonlm <clairtonlm@gmail.com>
2024-02-26 10:35:10 -0300

    Rodando

--- 15-Java-FullStack/04-NovaEscola/novaescola/Program.java
+++ 15-Java-FullStack/04-NovaEscola/novaescola/Program.java
@@ -8,50 +8,52 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("--------Aluno 01 NE--------");
         List<AlunoNE> alunos = new ArrayList<>();
-        for (int qtd = 0; qtd <= 1; qtd++) {
-        String nome = JOptionPane.showInputDialog("Digite o nome do Aluno: ");
-        String matricula = JOptionPane.showInputDialog("Qual a número da Matrícula do aluno:");
-        //String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento do aluno: ");
-        //String cpf = JOptionPane.showInputDialog("Qual a CPF do aluno: ");
-
         AlunoNE aluno1 = new AlunoNE();
+        for (int qtd = 0; qtd <= 1; qtd++) {
+            String nome = JOptionPane.showInputDialog("Digite o nome do Aluno: ");
+            String matricula = JOptionPane.showInputDialog("Qual a número da Matrícula do aluno:");
+            //String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento do aluno: ");
+            //String cpf = JOptionPane.showInputDialog("Qual a CPF do aluno: ");
 
-        aluno1.setNome(nome);
-        aluno1.setMatricula(Integer.valueOf(matricula));
-        //aluno1.setDataNascimento(dataNascimento);
-        //aluno1.setCpf(cpf);
+            aluno1 = new AlunoNE();
 
-        for (int pos = 1; pos <= 4; pos++) {
-            String nomeDisciplina = JOptionPane.showInputDialog("Digite o nome da disciplina " + pos + " ?");
-            String notaDisciplina = JOptionPane.showInputDialog("Digite a nota " + pos + " ?");
+            aluno1.setNome(nome);
+            aluno1.setMatricula(Integer.valueOf(matricula));
+            //aluno1.setDataNascimento(dataNascimento);
+            //aluno1.setCpf(cpf);
 
-            Disciplina disciplina = new Disciplina();
+            for (int pos = 1; pos <= 4; pos++) {
+                String nomeDisciplina = JOptionPane.showInputDialog("Digite o nome da disciplina " + pos + " ?");
+                String notaDisciplina = JOptionPane.showInputDialog("Digite a nota " + pos + " ?");
 
-            disciplina.setDisciplina(nomeDisciplina);
-            disciplina.setNota(Double.parseDouble(notaDisciplina));
+                Disciplina disciplina = new Disciplina();
 
-            aluno1.getDisciplinas().add(disciplina);
+                disciplina.setDisciplina(nomeDisciplina);
+                disciplina.setNota(Double.parseDouble(notaDisciplina));
 
-        }
-        int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
+                aluno1.getDisciplinas().add(disciplina);
 
-        if (escolha == 0) {//Yes
-            int continuarRemover = 0;
-            int posicao = 1;
+            }
+            int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
 
-            while (continuarRemover == 0) {
-                String disciplinaRemover = JOptionPane.showInputDialog("Qual a disciplina remover ? 1, 2, 3 ou 4 ");
-                aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - posicao);
-                posicao++;
-                continuarRemover = JOptionPane.showConfirmDialog(null, "Continuar a remover? ");
+            if (escolha == 0) {//Yes
+                int continuarRemover = 0;
+                int posicao = 1;
+
+                while (continuarRemover == 0) {
+                    String disciplinaRemover = JOptionPane.showInputDialog("Qual a disciplina remover ? 1, 2, 3 ou 4 ");
+                    aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - posicao);
+                    posicao++;
+                    continuarRemover = JOptionPane.showConfirmDialog(null, "Continuar a remover? ");
+                }
             }
         }
-    }
         //Impressão de dados
         System.out.println();
         System.out.println(aluno1.toString());
         System.out.println();
         System.out.println(aluno1.verificaAprovacao2());
+        System.out.println();
 
         System.out.println();
         System.out.println("_______FIM_______");
--- 15-Java-FullStack/04-NovaEscola/novaescola/Status.java
+++
@@ -1,4 +0,0 @@
-package novaescola;
-
-public enum Status {
-}
