6ad597484c66100c006846edefdcb63bbfc0181a
Clairtonlm <clairtonlm@gmail.com>
2024-02-28 21:10:02 -0300

    Treinando com o livro Java - Como Programar

--- 15-Java-FullStack/04-NovaEscola/novaescola/Program.java
+++ 15-Java-FullStack/04-NovaEscola/novaescola/Program.java
@@ -8,21 +8,23 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("--------Aluno 01 NE--------");
         List<AlunoNE> alunos = new ArrayList<>();
+
         AlunoNE aluno1 = new AlunoNE();
+
         for (int qtd = 0; qtd <= 1; qtd++) {
             String nome = JOptionPane.showInputDialog("Digite o nome do Aluno: ");
             String matricula = JOptionPane.showInputDialog("Qual a número da Matrícula do aluno:");
             //String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento do aluno: ");
             //String cpf = JOptionPane.showInputDialog("Qual a CPF do aluno: ");
 
-            aluno1 = new AlunoNE();
+            //aluno1 = new AlunoNE();
 
             aluno1.setNome(nome);
             aluno1.setMatricula(Integer.valueOf(matricula));
             //aluno1.setDataNascimento(dataNascimento);
             //aluno1.setCpf(cpf);
 
-            for (int pos = 1; pos <= 4; pos++) {
+            for (int pos = 1; pos <= 2; pos++) {
                 String nomeDisciplina = JOptionPane.showInputDialog("Digite o nome da disciplina " + pos + " ?");
                 String notaDisciplina = JOptionPane.showInputDialog("Digite a nota " + pos + " ?");
 
@@ -30,6 +32,7 @@ public class Program {
 
                 disciplina.setDisciplina(nomeDisciplina);
                 disciplina.setNota(Double.parseDouble(notaDisciplina));
+
 
                 aluno1.getDisciplinas().add(disciplina);
 
