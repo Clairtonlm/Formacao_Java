4bbeb6581d320f9c7a0a92cff8df512f507203e9
Clairtonlm <clairtonlm@gmail.com>
2024-02-28 21:27:49 -0300

    Fazendo calculo

--- 06-Pratica_Java/Calc/src/model/Calc.java
+++ 06-Pratica_Java/Calc/src/model/Calc.java
@@ -1,4 +1,18 @@
 package model;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Calc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.UK);
+        
+        System.out.print("Digite o primeiro número");
+        int num1 = sc.nextInt();
+        System.out.print("Digite o segundo número");
+        int num2 = sc.nextInt();
+        
+        int sum = num1 + num2;
+        System.out.println("A soma dos numeros " + sum);
+    }
 }
