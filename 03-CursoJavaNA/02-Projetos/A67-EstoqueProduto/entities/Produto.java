d43fbbfd5d799ac3319d374fa89b18c0e8ec1cc8
Clairtonlm <clairtonlm@gmail.com>
2024-11-19 10:21:19 -0300

    Construtor - permitit ou obrigar iniciar valores

--- 03-CursoJavaNA/02-Projetos/A67-EstoqueProduto/entities/Produto.java
+++ 03-CursoJavaNA/02-Projetos/A67-EstoqueProduto/entities/Produto.java
@@ -16,6 +16,12 @@ public class Produto {
         this.quantidade = quantidade;
     }
 
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = 0;
+    }
+
     // Method to calculate total value in stock
     public double totalValueInStock() {
         return preco * quantidade;
