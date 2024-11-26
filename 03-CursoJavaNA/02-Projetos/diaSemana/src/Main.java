c67cc1f08c0f0a75b72f015220aee3e9b39337f2
Clairtonlm <clairtonlm@gmail.com>
2024-11-26 09:51:09 -0300

    Caso swithc

---
+++ 03-CursoJavaNA/02-Projetos/diaSemana/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        int dayOffWeek = 3; // Exemplo de valor para o dia da semana
+        String day;
+
+        switch (dayOffWeek) {
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+                day = "Saturday";
+                break;
+            case 7:
+                day = "Sunday";
+                break;
+            default:
+                day = "Invalid day";
+                break;
+        }
+
+        System.out.println(day);
+    }
+}
